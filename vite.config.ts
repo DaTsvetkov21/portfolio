77eb3b92bb9c34821f9e24f583994bedc00739ea
DATsvetkov <d.tsvetkov@napopravku.ru>
2023-11-29 17:05:27 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import {defineConfig, loadEnv} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import {viteSingleFile} from "vite-plugin-singlefile";
+
+const DEFAULT_PORT = 8080;
+
+// @ts-ignore
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    server: {
+      host: env.DEV_DOMAIN,
+      port: Number(env.DEV_PORT || DEFAULT_PORT),
+    },
+    build: {
+      sourcemap: true,
+    },
+    plugins: [
+
+      vue(),
+      viteSingleFile(),
+      Components({
+        dts: true,
+        resolvers: [],
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /^@\/(.+)/,
+          replacement: path.resolve(path.resolve(__dirname), 'src') + '/$1'
+        },
+      ]
+    },
+    optimizeDeps: {
+      include: [
+        'lodash-es',
+      ],
+    },
+  });
+};
